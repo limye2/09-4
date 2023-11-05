3c5a7146d7163604607e444b516101e5dfbcb8e4
limye2 <144992260+limye2@users.noreply.github.com>
2023-11-05 23:24:49 +0900

    Initial Commit

---
+++ 09-4/main.c
@@ -0,0 +1,37 @@
+//
+//  main.c
+//  09-4
+//
+//  Created by MacBook Air on 2023/11/05.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 4
+
+void square_array(int a[], int size)
+{
+    int i;
+    
+    for(i=0; i<size; i++)
+        a[i]=a[i]*a[i];
+}
+
+void print_array(int a[], int size)
+{
+    int i;
+    
+    for(i=0; i<size; i++)
+        printf("%3d", a[i]);
+    printf("\n");
+    
+}
+
+int main(int argc, const char * argv[]) {
+    int list[SIZE] = {1, 2, 3, 4};
+    
+    print_array(list, SIZE);
+    square_array(list, SIZE);
+    print_array(list, SIZE);
+    return 0;
+}
